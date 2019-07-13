8764e87d9c0925e559758b279b9e88ba479a4ab0
Caitlin Schrader <c.n.schrader22@gmail.com>
2019-07-13 18:48:33 -0500

    added a button that will start a timer for the coffee

---
+++ mern_boilerplate-master/client/src/components/TimerButton/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Button } from 'react-materialize';
+
+
+
+function button(){
+    return(
+        <div>
+<Button waves="light" style={{marginRight: '5px'}}>
+button
+</Button>
+</div>
+
+    )
+}
+
+export default button;
