34ea268f314a2e4da1910db0ad98301c09b865a8
Caitlin Schrader <c.n.schrader22@gmail.com>
2019-07-15 17:24:36 -0500

    added react router and successfully rendering the welcome page at the root level.

--- mern_boilerplate-master/client/src/App.js
+++ mern_boilerplate-master/client/src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Welcome from './pages/Welcome'
 
-import Navbar from './components/Navbar/index';
-import Card from './components/card';
-import DiffCard from './components/difficultyCard/index';
-import Button from './components/TimerButton/index'
+// import Navbar from './components/Navbar/index';
+// import Card from './components/card';
+// import DiffCard from './components/difficultyCard/index';
+// import Button from './components/TimerButton/index';
+// import SearchBar from './components/Searchbar/searchbar.js'
+
 
 
 
 function App() {
   return (
-    <React.Fragment>
-    <Navbar />
-   
-
-    
-    
-    </React.Fragment>
+    <Router>
+      <React.Fragment>
+        <Route exact path='/' component={Welcome} />
+      </React.Fragment>
+    </Router>
 
   );
 }
