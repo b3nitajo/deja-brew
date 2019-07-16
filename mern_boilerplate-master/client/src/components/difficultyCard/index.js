ec696b8c5145af479205b6ca69b08604a7de98b9
Caitlin Schrader <c.n.schrader22@gmail.com>
2019-07-16 18:11:22 -0500

    change the difficultyCard component's structure a little to contain the Links

--- mern_boilerplate-master/client/src/components/difficultyCard/index.js
+++ mern_boilerplate-master/client/src/components/difficultyCard/index.js
@@ -1,43 +1,52 @@
 import React from 'react';
 import { Row, Col, CardPanel, Container } from 'react-materialize';
+import { Link } from 'react-router-dom';
 import 'components/difficultyCard/diff.css'
 
-function diffCard(){
-    return(
+function diffCard() {
+    return (
         <React.Fragment>
-            <Container>
-        <Row>
-<Col className="diffCard" m={3} s={12}>
-<CardPanel className="teal">
-<span className="white-text">
-<p>Easy</p>
-</span>
-</CardPanel>
-</Col>
-
-
-
-
-<Col className="diffCard" m={3} s={12}>
-<CardPanel className="teal">
-<span className="white-text">
-<p>Medium</p>
-</span>
-</CardPanel>
-</Col>
-
-
-
-<Col className="diffCard" m={3} s={12}>
-<CardPanel className="teal">
-<span className="white-text">
-<p>Hard</p>
-</span>
-</CardPanel>
-</Col>
-</Row>
-</Container>
-</React.Fragment>
+            
+               
+                    <Container>
+                        <Row>
+                        <Col className="diffCard" m={3} s={12}>
+                            <Link>
+                        <CardPanel className="teal">
+                            <span className="white-text">
+                                <p>Easy</p>
+                            </span>
+                        </CardPanel>
+                        </Link>
+                        </Col>
+                        
+                        
+                        <Col className="diffCard" m={3} s={12}>
+                            <Link>
+                        <CardPanel className="teal">
+                            <span className="white-text">
+                                <p>Easy</p>
+                            </span>
+                        </CardPanel>
+                        </Link>
+                        </Col>
+                        
+                        
+                        <Col className="diffCard" m={3} s={12}>
+                            <Link>
+                        <CardPanel className="teal">
+                            <span className="white-text">
+                                <p>Easy</p>
+                            </span>
+                        </CardPanel>
+                        </Link>
+                        </Col>
+                        </Row>
+                        </Container>
+                  
+                
+            
+        </React.Fragment>
 
     )
 }
