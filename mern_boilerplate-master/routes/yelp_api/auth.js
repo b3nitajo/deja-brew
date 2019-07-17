8e87ca695133133263b4092fcccbc876047c73e8
Benita Jones <47437893+b3nitajo@users.noreply.github.com>
2019-07-17 00:51:45 -0500

    yelp testing

--- mern_boilerplate-master/routes/yelp_api/auth.js
+++ mern_boilerplate-master/routes/yelp_api/auth.js
@@ -1,11 +1,27 @@
 const router = require('express').Router();
-const passport = require('../../config/passport');
 const authController = require('../../controllers/auth');
 
-router.use(passport.authenticate('local'));
 
 // Matches with '/api/auth/login'
 router.route('/login')
   .post(authController.login);
 
 module.exports = router;
+
+
+
+
+axios.defaults.baseURL = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=coffee&location=';
+axios.defaults.headers.common['Authorization'] = process.env.YELP_KEY;
+axios.defaults.headers.post['X-Requested-With'] = 'XMLHttpRequest';
+//'X-Requested-With': 'XMLHttpRequest'
+//const BASEURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=coffee&location=";
+//const limitReturn = "&limit=3";
+// Export an object with a "search" method that searches the Giphy API for the passed query
+
+
+export default { 
+  search: function(query) {
+  return axios.get(query);
+  }
+};
--- mern_boilerplate-master/routes/yelp_api/index.js
+++ mern_boilerplate-master/routes/yelp_api/index.js
@@ -1,6 +1,26 @@
-const router = require('express').Router();
-const authRoutes = require('./auth');
+const axios = require('axios');
+//const router = require('express').Router();
+//const authRoutes = require('./auth');
 
-router.use('/auth', authRoutes);
+//router.use('/auth', authRoutes);
 
-module.exports = router;
+axios.defaults.baseURL = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=coffee&location=';
+axios.defaults.headers.common['Authorization'] = process.env.YELP_KEY;
+axios.defaults.headers.post['X-Requested-With'] = 'XMLHttpRequest';
+
+//module.exports = router;
+//import axios from "axios";
+//const axios = require('axios');
+// axios.defaults.baseURL = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=coffee&location=';
+// axios.defaults.headers.common['Authorization'] = process.env.YELP_KEY;
+// axios.defaults.headers.post['X-Requested-With'] = 'XMLHttpRequest';
+//const BASEURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=coffee&location=";
+//const limitReturn = "&limit=3";
+// Export an object with a "search" method that searches the Giphy API for the passed query
+
+
+export default { 
+  search: function(query) {
+  return axios.get(query);
+  }
+};
