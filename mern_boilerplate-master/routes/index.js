0297de8d13799f9fac4040c17846efdd1d87ca7b
ArchonQ <brandon.cypaso@gmail.com>
2019-07-18 01:03:26 -0500

    first commmit in mongo

--- mern_boilerplate-master/routes/index.js
+++ mern_boilerplate-master/routes/index.js
@@ -1,7 +1,13 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
+const path = require("path");
+const router = require("express").Router();
+const methodRoutes = require("./method");
 
 // API Routes
-router.use('/api/v1', apiRoutes);
+router.use("/method", methodRoutes);
+
+// If no API routes are hit, send the React app
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 module.exports = router;
