4c0c652801b9e96b192e4d2c52f01556855e8036
Caitlin Schrader <c.n.schrader22@gmail.com>
2019-07-17 23:46:30 -0500

    got data to render on cards, but need to change state method a little for reset

--- mern_boilerplate-master/client/src/components/card/index.js
+++ mern_boilerplate-master/client/src/components/card/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Row, Col, Card } from 'react-materialize';
 
 
-function card(){
+function card(props){
 return (
 <React.Fragment>
 <Row>
@@ -10,17 +10,13 @@ return (
 <Card
   className="blue-grey darken-1"
   textClassName="white-text"
-  title="Recipe"
+  title={props.name}
   actions={[<a />,<a />]}
 >
-Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-Phasellus varius odio vel diam aliquet, a eleifend lacus ullamcorper.
-Suspendisse a lectus tincidunt neque condimentum ultricies.
-Aenean scelerisque urna non turpis consequat molestie eget eget leo.
-Mauris rutrum tellus quis tellus lobortis sodales.
-Duis sed ipsum rhoncus arcu volutpat interdum id quis lorem.
-Vestibulum congue enim fermentum nibh consequat, ut interdum erat rutrum.
-Nam faucibus ligula a sapien posuere mollis.
+<ul>
+    <li>{props.coffee}</li>
+    <li>{props.milk}</li>
+</ul>
 </Card>
 </Col>
 </Row>
